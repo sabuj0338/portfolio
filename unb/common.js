413c1fd7d7b581c7c4ab17315a65ad8f7ab3af11
Sabuj Islam <sabuj0338@gmail.com>
2022-06-11 10:27:39 +0600

    updates

--- unb/common.js
+++ unb/common.js
@@ -1,7 +1,7 @@
 function alpinejs() {
     // console.log("👍common js initiated👍");
     return {
-        isDrawerOpen: false,
+        isDrawerOpen: true,
         openDrawer() {
             this.isDrawerOpen = true;
             this.$nextTick(() => {
@@ -42,3 +42,46 @@ right.addEventListener('click', (event) => {
 left.addEventListener('click', (event) => {
     menu.scrollLeft -= scroll;
 });
+
+// carousel slider of photos
+
+const carousel = (function () {
+    const carouselItem = document.getElementsByName("carousel");
+    function next() {
+        const carousel = document.querySelector('input[type=radio][name=carousel]:checked');
+        const id = parseInt(carousel.value.split("-")[1]);
+        carousel.checked = false;
+        document.getElementById("carousel-item-"+id).style.display = 'none';
+
+        if(id < (carouselItem.length - 1)) {
+            document.getElementById("carousel-"+(id+1)).checked = true;
+            document.getElementById("carousel-item-"+(id+1)).style.display = 'block';
+        } else {
+            document.getElementById("carousel-0").checked = true;
+            document.getElementById("carousel-item-0").style.display = 'block';
+        }
+    }
+
+    function prev() {
+        const carousel = document.querySelector('input[type=radio][name=carousel]:checked');
+        const id = parseInt(carousel.value.split("-")[1]);
+        carousel.checked = false;
+        document.getElementById("carousel-item-"+id).style.display = 'none';
+
+        if(id <= 0) {
+            document.getElementById("carousel-" + (carouselItem.length - 1)).checked = true;
+            document.getElementById("carousel-item-"+ (carouselItem.length - 1)).style.display = 'block';
+        } else {
+            document.getElementById("carousel-" + (id - 1)).checked = true;
+            document.getElementById("carousel-item-" + (id - 1)).style.display = 'block';
+        }
+    }
+
+    return {
+        length: carouselItem.length,
+        next: next,
+        prev: prev
+    }
+    
+})();
+setInterval(carousel.next, 5000);
