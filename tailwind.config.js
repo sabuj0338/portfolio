546682269023cdbd88784410d45a4a460ca5e886
Sabuj Islam <sabuj0338@gmail.com>
2022-09-26 12:02:32 +0600

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,27 @@ module.exports = {
         extend: {},
     },
     plugins: [],
+    theme: {
+        extend: {
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                'primary': '#6ef3a4',
+                'background-light': '#2d2e32',
+                'background': '#25262a',
+                'white': '#ffffff',
+                'purple': '#3f3cbb',
+                'midnight': '#121063',
+                'metal': '#565584',
+                'tahiti': '#3ab7bf',
+                'silver': '#ecebff',
+                'bubble-gum': '#ff77e9',
+                'bermuda': '#78dcca',
+            },
+        },
+        fontFamily: {
+            'sans': ['Roboto Mono'],
+            'roboto': ['Roboto Mono'],
+        }
+    },
 }
