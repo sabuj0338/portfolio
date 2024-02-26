d4a53bb073c6b138fe700384a89ab01afa54c840
Sabuj Islam <sabuj0338@gmail.com>
2024-02-26 15:41:24 +0600

    api endpoint in now .env

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -1,7 +1,7 @@
 
 export default async function getData(): Promise<undefined | IDataResponse> {
   try {
-    const res = await fetch("https://sabuj0338.vercel.app/data.json");
+    const res = await fetch(`${import.meta.env.VITE_API_ENDPOINT}/data.json`);
     if(!res.ok) throw new Error();
     const out = await res.json();
     return out;
