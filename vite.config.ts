2a1f38f872c9ab235cb50e15acd02499f2010219
Sabuj Islam <sabuj0338@gmail.com>
2024-02-26 10:37:24 +0600

    vercel and github configuration done

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio/",
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/portfolio/",
 })
